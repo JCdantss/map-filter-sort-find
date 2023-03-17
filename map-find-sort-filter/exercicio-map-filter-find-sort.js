42729eb561fff3864cd98cfa84fdd8cb55816064
Julio Cesar <juliocesardantss@gmail.com>
2023-03-17 13:51:12 -0300

    primeiro commit

---
+++ map-find-sort-filter/exercicio-map-filter-find-sort.js
@@ -0,0 +1,59 @@
+
+const alunas = [
+    { nome: "Ashley", prova: { p1: 5.6, p2: 6.7, p3: 9 } },
+    { nome: "Sabrina", prova: { p1: 6.3, p2: 7.5, p3: 10 } },
+    { nome: "Samantha", prova: { p1: 8, p2: 9.2, p3: 7 } },
+    { nome: "Andreia", prova: { p1: 9, p2: 8, p3: 10 } },
+    { nome: "Raquel", prova: { p1: 10, p2: 9.7, p3: 5 } },
+    { nome: "Amanda", prova: { p1: 2, p2: 4.6, p3: 9.9 } },
+    { nome: "Pietra", prova: { p1: 8.3, p2: 3.1, p3: 9.8 } },
+    { nome: "Jaqueline", prova: { p1: 3.4, p2: 7.2, p3: 6.8 } },
+    { nome: "Alessandra", prova: { p1: 1.4, p2: 2.7, p3: 6.9 } },
+    { nome: "Jane Kelly", prova: { p1: 7, p2: 5.5, p3: 9.1 } },
+]
+
+
+//1) FUNCAO PARA CALCUCAR MEDIAS DAS ALUNAS
+
+
+//==================================================================
+
+//2) FUNCAO CORINGA PARA CALCULAR MEDIAS
+
+ //talvez uma funcao de media que retorne true ou flase tipo essa aqui da linha 24
+
+//==================================================================
+
+//3) Fazer uma função que retorne um array de nomes das aprovadas
+
+
+//==================================================================
+
+//4) Fazer uma função que retorne um array de nome das reprovadas
+
+
+
+//==================================================================
+
+//5) Fazer uma função que retorne um array de objetos:
+
+
+
+//==================================================================
+
+//6) ordenar as notas das alunas
+
+
+//==================================================================
+
+//7) Fazer uma função que retorne o nome da aluna com maior nota
+
+
+//==================================================================
+
+//8) Fazer uma função que retorne o nome da aluna com menor nota
+
+
+//==================================================================
+
+//9) Fazer uma função que retorne a media de toda a turma
